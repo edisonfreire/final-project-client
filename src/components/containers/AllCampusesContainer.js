45f320221d5782a507700c1223d36c5f6490d314
Edison <edisonfreire14@gmail.com>
2024-12-13 15:53:15 -0500

    improvements to all campus view and container

--- src/components/containers/AllCampusesContainer.js
+++ src/components/containers/AllCampusesContainer.js
@@ -9,13 +9,12 @@ import Header from './Header';
 import { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchAllCampusesThunk } from "../../store/thunks";
+import { fetchAllCampusesThunk, deleteCampusThunk } from "../../store/thunks";
 import { AllCampusesView } from "../views";
 
 class AllCampusesContainer extends Component {
   // Get all campuses data from back-end database
   componentDidMount() {
-    console.log(this.props);
     this.props.fetchAllCampuses();
   }
 
@@ -26,35 +25,34 @@ class AllCampusesContainer extends Component {
         <Header />
         <AllCampusesView
           allCampuses={this.props.allCampuses}
+          deleteCampus={this.props.deleteCampus} // Pass deleteCampus function as a prop
         />
       </div>
     );
   }
 }
 
-// 1. The "mapState" argument specifies the data from Redux Store that the component needs.
-// The "mapState" is called when the Store State changes, and it returns a data object of "allCampuses".
-// The following 2 input arguments are passed to the "connect" function used by "AllCampusesContainer" component to connect to Redux Store.
+// Map state from Redux store to props
 const mapState = (state) => {
   return {
-    allCampuses: state.allCampuses,  // Get the State object from Reducer "allCampuses"
+    allCampuses: state.allCampuses,
   };
-};  
-// 2. The "mapDispatch" argument is used to dispatch Action (Redux Thunk) to Redux Store.
-// The "mapDispatch" calls the specific Thunk to dispatch its action. The "dispatch" is a function of Redux Store.
+};
+
+// Map dispatch actions to props
 const mapDispatch = (dispatch) => {
   return {
     fetchAllCampuses: () => dispatch(fetchAllCampusesThunk()),
+    deleteCampus: (id) => dispatch(deleteCampusThunk(id)), // Map deleteCampusThunk
   };
 };
 
-// Type check props;
+// Type check props
 AllCampusesContainer.propTypes = {
   allCampuses: PropTypes.array.isRequired,
   fetchAllCampuses: PropTypes.func.isRequired,
+  deleteCampus: PropTypes.func.isRequired,
 };
 
 // Export store-connected container by default
-// AllCampusesContainer uses "connect" function to connect to Redux Store and to read values from the Store 
-// (and re-read the values when the Store State updates).
 export default connect(mapState, mapDispatch)(AllCampusesContainer);
---
+++ src/components/containers/NewCampusContainer.js
@@ -0,0 +1,105 @@
+/*==================================================
+NewCampusContainer.js
+
+The Container component is responsible for stateful logic and data fetching, and
+passes data (if any) as props to the corresponding View component.
+If needed, it also defines the component's "connect" function.
+================================================== */
+import Header from "./Header";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+import NewCampusView from "../views/NewCampusView";
+import { addCampusThunk } from "../../store/thunks";
+
+class NewCampusContainer extends Component {
+  // Initialize state
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      address: "",
+      description: "",
+      imageUrl: "",
+      errors: [], // Track validation errors
+      redirect: false,
+      redirectId: null,
+    };
+  }
+
+  // Capture input data when it is entered
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Take action after user click the submit button
+  handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent browser reload/refresh after submit.
+
+    let campus = {
+      name: this.state.name,
+      address: this.state.address,
+      description: this.state.description,
+      imageUrl: this.state.imageUrl || undefined, // Optional field
+    };
+    try {
+      // Add new campus in back-end database
+      let newCampus = await this.props.addCampus(campus);
+
+      // Update state, and trigger redirect to show the new campus
+      this.setState({
+        name: "",
+        address: "",
+        description: "",
+        imageUrl: "",
+        errors: [],
+        redirect: true,
+        redirectId: newCampus.id,
+      });
+    } catch (errors) {
+      if (Array.isArray(errors)) {
+        this.setState({ errors });
+      } else {
+        alert(errors.message || "An unexpected error occurred.");
+      }
+    }
+  };
+
+  // Unmount when the component is being removed from the DOM:
+  componentWillUnmount() {
+    this.setState({ redirect: false, redirectId: null });
+  }
+
+  // Render new campus input form
+  render() {
+    // Redirect to new campus's page after submit
+    if (this.state.redirect) {
+      return <Redirect to={`/campus/${this.state.redirectId}`} />;
+    }
+
+    // Display the input form via the corresponding View component
+    return (
+      <div>
+        <Header />
+        <NewCampusView
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          errors={this.state.errors}
+        />
+      </div>
+    );
+  }
+}
+
+// Map the addCampusThunk to props
+const mapDispatch = (dispatch) => {
+  return {
+    addCampus: (campus) => dispatch(addCampusThunk(campus)),
+  };
+};
+
+// Export store-connected container by default
+export default connect(null, mapDispatch)(NewCampusContainer);
